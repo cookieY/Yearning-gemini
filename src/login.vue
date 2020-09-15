5f29bb1910a47e797522eb536f2ee9ed752c18cb
HenryYee <henry@yearning.io>
2020-09-15 16:27:51 +0800

    修复用户无法注册的问题

--- src/login.vue
+++ src/login.vue
@@ -1,31 +1,31 @@
 <style lang="css" scoped>
-    @import "styles/bootstrap.min.css";
-    @import "styles/login.css";
+@import "styles/bootstrap.min.css";
+@import "styles/login.css";
 
-    .div-relative {
-        position: relative;
-        width: 100%;
-        height: 100%;
-    }
+.div-relative {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
 
-    .div-a {
-        position: absolute;
-        z-index: 1000;
-        width: 100%;
-    }
+.div-a {
+    position: absolute;
+    z-index: 1000;
+    width: 100%;
+}
 
-    li {
-        float: left;
-        list-style-type: none;
-    }
+li {
+    float: left;
+    list-style-type: none;
+}
 
-    a:link {
-        color: #FFFFFF;
-    }
+a:link {
+    color: #FFFFFF;
+}
 
-    a:visited {
-        color: #FFFFFF;
-    }
+a:visited {
+    color: #FFFFFF;
+}
 </style>
 
 <template>
@@ -34,14 +34,14 @@
             <div class="container">
                 <div>
                     <a class="navbar-brand">
-                        {{$t('version')}}: v2.3.0 Interstellar
+                        {{ $t('version') }}: v2.3.0 Interstellar
                     </a>
-                    <Button v-if="switchCode" type="default" ghost @click="register= true"> {{$t('sign')}}</Button>
+                    <Button v-if="switchCode" type="default" ghost @click="register= true"> {{ $t('sign') }}</Button>
                 </div>
                 <div>
                     <ul>
                         <li>
-                            <Button type="default" @click="sponsorship = true" ghost>{{$t('sponsor')}}</Button>
+                            <Button type="default" @click="sponsorship = true" ghost>{{ $t('sponsor') }}</Button>
                         </li>
                         <li>
                             <div style="margin-left: 50%">
@@ -84,12 +84,12 @@
                                        v-model="formInline.code"
                                        @keyup.enter="signIn"/>
                                 <SIdentify
-                                        @identifyCode="checkCode" class="input-group-addon" :replace="replace"></SIdentify>
+                                    @identifyCode="checkCode" class="input-group-addon" :replace="replace"></SIdentify>
                             </div>
                             <div class="footer text-center">
-                                <Checkbox v-model="single"> {{$t('ldap')}}</Checkbox>
+                                <Checkbox v-model="single"> {{ $t('ldap') }}</Checkbox>
                                 <a class="btn btn-primary btn-round btn-lg btn-block" @click="signIn()">
-                                    {{$t('login')}}</a>
+                                    {{ $t('login') }}</a>
                             </div>
                         </form>
                     </div>
@@ -102,25 +102,25 @@
                             <li>
                                 <Tooltip content="yearning.io" placement="top-start">
                                     <a class="nav-link" href="https://yearning.io">
-                                        {{$t('about')}}
+                                        {{ $t('about') }}
                                     </a>
                                 </Tooltip>
                             </li>
                             <li>
                                 <Tooltip :content="$t('community_addr')" placement="top-start">
                   <span class="nav-link">
-                   {{$t('community')}}
+                   {{ $t('community') }}
                   </span>
                                 </Tooltip>
                             </li>
                             <li>
                                 <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" class="nav-link">
-                                    {{$t('license')}}
+                                    {{ $t('license') }}
                                 </a>
                             </li>
                         </ul>
                     </nav>
-                    <div class="copyright"> {{$t('tips')}}</div>
+                    <div class="copyright"> {{ $t('tips') }}</div>
                 </div>
             </footer>
         </div>
@@ -155,18 +155,18 @@
         </Modal>
 
         <Modal v-model="sponsorship" :title="$t('sponsor_title')" width="640">
-            <h3> {{$t('sponsor_1')}}</h3>
-            {{$t('sponsor_2')}}
+            <h3> {{ $t('sponsor_1') }}</h3>
+            {{ $t('sponsor_2') }}
             <br>
             <br>
-            <h3>{{$t('sponsor_3')}}</h3>
-            {{$t('sponsor_4')}}
+            <h3>{{ $t('sponsor_3') }}</h3>
+            {{ $t('sponsor_4') }}
             <br>
             <br>
-            <h3>{{$t('sponsor_5')}}</h3>
-            {{$t('sponsor_6')}}
+            <h3>{{ $t('sponsor_5') }}</h3>
+            {{ $t('sponsor_6') }}
             <br>
-            {{$t('sponsor_7')}}
+            {{ $t('sponsor_7') }}
             <br>
             <br>
             <img height="300" width="300" src="./assets/alipay.jpg">
@@ -175,182 +175,182 @@
     </div>
 </template>
 <script lang="ts">
-    import SIdentify from '@/components/identify.vue'
-    import {Mixins, Component} from "vue-property-decorator";
-    import i18n from '@/language/index';
-    import att_mixins from "@/mixins/basic";
+import SIdentify from '@/components/identify.vue'
+import {Mixins, Component} from "vue-property-decorator";
+import i18n from '@/language/index';
+import att_mixins from "@/mixins/basic";
 
-    @Component({components: {SIdentify}})
-    export default class login extends Mixins(att_mixins) {
+@Component({components: {SIdentify}})
+export default class login extends Mixins(att_mixins) {
 
-        valideuserinfoPassword = (rule: any, value: string, callback: any) => {
-            if (value !== this.userinfo.password) {
-                callback(new Error(i18n.t('sign_up_validate.twice') as any))
-            } else {
-                callback()
-            }
-        };
-        regExp_password = (rule: any, value: string, callback: any) => {
-            let pPattern = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$/;
-            if (!pPattern.test(value)) {
-                callback(new Error(i18n.t('sign_up_validate.regexp') as any))
-            } else {
-                callback()
-            }
-        };
-        replace = false
-        single = false;
-        switchCode = false;
-        sponsorship = false;
-        register = false;
-        userinfo = {
-            username: '',
-            password: '',
-            confirm_password: '',
-            mail: '',
-            real_name: '',
-            department: ''
-        };
-        userInfoValidate = {
-            username: [
-                {
-                    required: true,
-                    message: i18n.t('sign_up_validate.username'),
-                    trigger: 'blur'
-                }
-            ],
-            password: [
-                {
-                    required: true,
-                    message: i18n.t('sign_up_validate.password'),
-                    trigger: 'blur'
-                },
-                {
-                    min: 6,
-                    message: i18n.t('sign_up_validate.min'),
-                    trigger: 'blur'
-                },
-                {
-                    max: 32,
-                    message: i18n.t('sign_up_validate.max'),
-                    trigger: 'blur'
-                },
-                {
-                    validator: this.regExp_password,
-                    trigger: 'blur'
-                }
-            ],
-            confirmpassword: [
-                {
-                    required: true,
-                    message: i18n.t('sign_up_validate.confirm'),
-                    trigger: 'blur'
-                },
-                {
-                    validator: this.valideuserinfoPassword,
-                    trigger: 'blur'
-                }
-            ],
-            realname: [
-                {
-                    required: true,
-                    message: i18n.t('sign_up_validate.real'),
-                    trigger: 'blur'
-                }
-            ],
-            department: [
-                {
-                    required: true,
-                    message: i18n.t('sign_up_validate.department'),
-                    trigger: 'blur'
-                }
-            ],
-            mail: [
-                {required: true, message: i18n.t('sign_up_validate.mail'), trigger: 'blur'},
-                {type: 'email', message: i18n.t('sign_up_validate.mail_format'), trigger: 'blur'}
-            ]
-        };
-        formInline = {
-            user: '',
-            password: '',
-            code: ''
-        };
-        check_code = '';
-
-        checkCode(vl: string) {
-            this.check_code = vl.toLowerCase()
+    valideuserinfoPassword = (rule: any, value: string, callback: any) => {
+        if (value !== this.userinfo.password) {
+            callback(new Error(i18n.t('sign_up_validate.twice') as any))
+        } else {
+            callback()
         }
-
-        LoginRegister() {
-            let is_validate: any = this.$refs['user_reg'];
-            is_validate.validate((valid: boolean) => {
-                if (valid) {
-                    this.$http.post(this.$config.register, {
-                        'user_info': this.userinfo
-                    })
-                        .then((res: { data: string; }) => {
-                            this.$config.notice(res.data);
-                        })
-                        .catch((error: any) => {
-                            this.$config.err_notice(this, error)
-                        })
-                } else {
-                    this.$config.notice(i18n.t('sign_up_validate.sign_fail') as string)
-                }
-            })
-            this.resetFields('user_reg')
+    };
+    regExp_password = (rule: any, value: string, callback: any) => {
+        let pPattern = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$/;
+        if (!pPattern.test(value)) {
+            callback(new Error(i18n.t('sign_up_validate.regexp') as any))
+        } else {
+            callback()
         }
-
-        signIn() {
-            if (this.check_code !== this.formInline.code.toLowerCase()) {
-                this.$Message.warning({
-                    content: i18n.t('sign_up_validate.pin') as string
-                });
-                this.replace = !this.replace
-                return
+    };
+    replace = false
+    single = false;
+    switchCode = false;
+    sponsorship = false;
+    register = false;
+    userinfo = {
+        username: '',
+        password: '',
+        confirm_password: '',
+        mail: '',
+        real_name: '',
+        department: ''
+    };
+    userInfoValidate = {
+        username: [
+            {
+                required: true,
+                message: i18n.t('sign_up_validate.username'),
+                trigger: 'blur'
             }
-            let url = this.$config.auth;
-            if (this.single) {
-                url = `${this.$config.gen}/ldap`
+        ],
+        password: [
+            {
+                required: true,
+                message: i18n.t('sign_up_validate.password'),
+                trigger: 'blur'
+            },
+            {
+                min: 6,
+                message: i18n.t('sign_up_validate.min'),
+                trigger: 'blur'
+            },
+            {
+                max: 32,
+                message: i18n.t('sign_up_validate.max'),
+                trigger: 'blur'
+            },
+            {
+                validator: this.regExp_password,
+                trigger: 'blur'
             }
-            this.$http.post(url, {
-                'username': this.formInline.user,
-                'password': this.formInline.password
-            })
-                .then((res: { data: { [x: string]: any; token: string; }; }) => {
-                    this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
-                    sessionStorage.setItem('user', this.formInline.user);
-                    sessionStorage.setItem('jwt', `Bearer ${res.data.token}`);
-                    sessionStorage.setItem('auth', res.data['permissions']);
-                    sessionStorage.setItem('real_name', res.data['real_name']);
-                    let auth = res.data['permissions'];
-                    if (auth === 'guest') {
-                        sessionStorage.setItem('access', '1')
-                    } else if (auth === 'admin' || auth === 'perform') {
-                        sessionStorage.setItem('access', '2')
-                    } else if (auth === 'super') {
-                        sessionStorage.setItem('access', '3')
-                    }
-                    this.$router.push({
-                        name: 'home_index'
-                    })
-                })
-                .catch((err: any) => {
-                    this.replace = !this.replace
-                    this.$config.auth_notice(err)
-                })
-        }
+        ],
+        confirmpassword: [
+            {
+                required: true,
+                message: i18n.t('sign_up_validate.confirm'),
+                trigger: 'blur'
+            },
+            {
+                validator: this.valideuserinfoPassword,
+                trigger: 'blur'
+            }
+        ],
+        realname: [
+            {
+                required: true,
+                message: i18n.t('sign_up_validate.real'),
+                trigger: 'blur'
+            }
+        ],
+        department: [
+            {
+                required: true,
+                message: i18n.t('sign_up_validate.department'),
+                trigger: 'blur'
+            }
+        ],
+        mail: [
+            {required: true, message: i18n.t('sign_up_validate.mail'), trigger: 'blur'},
+            {type: 'email', message: i18n.t('sign_up_validate.mail_format'), trigger: 'blur'}
+        ]
+    };
+    formInline = {
+        user: '',
+        password: '',
+        code: ''
+    };
+    check_code = '';
 
-        mounted() {
-            let windows: any = window;
-            windows.particlesJS.load('band', `${process.env.BASE_URL}particlesjs-config.json`);
-            this.$http.get(`${this.$config.gen}/fetch`)
-                .then((res: { data: { reg: number; }; }) => {
-                    if (res.data.reg === 1) {
-                        this.switchCode = true;
-                    }
-                })
-                .catch((err: any) => this.$config.err_notice(this,err));
-        }
+    checkCode(vl: string) {
+        this.check_code = vl.toLowerCase()
     }
+
+    LoginRegister() {
+        let is_validate: any = this.$refs['user_reg'];
+        is_validate.validate((valid: boolean) => {
+            if (valid) {
+                this.$http.post(this.$config.register, {
+                    'user_info': this.userinfo
+                })
+                    .then((res: { data: string; }) => {
+                        this.$config.notice(res.data);
+                    })
+                    .catch((error: any) => {
+                        this.$config.err_notice(this, error)
+                    })
+                    .finally(() => this.resetFields('user_reg'))
+            } else {
+                this.$config.notice(i18n.t('sign_up_validate.sign_fail') as string)
+            }
+        })
+    }
+
+    signIn() {
+        if (this.check_code !== this.formInline.code.toLowerCase()) {
+            this.$Message.warning({
+                content: i18n.t('sign_up_validate.pin') as string
+            });
+            this.replace = !this.replace
+            return
+        }
+        let url = this.$config.auth;
+        if (this.single) {
+            url = `${this.$config.gen}/ldap`
+        }
+        this.$http.post(url, {
+            'username': this.formInline.user,
+            'password': this.formInline.password
+        })
+            .then((res: { data: { [x: string]: any; token: string; }; }) => {
+                this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+                sessionStorage.setItem('user', this.formInline.user);
+                sessionStorage.setItem('jwt', `Bearer ${res.data.token}`);
+                sessionStorage.setItem('auth', res.data['permissions']);
+                sessionStorage.setItem('real_name', res.data['real_name']);
+                let auth = res.data['permissions'];
+                if (auth === 'guest') {
+                    sessionStorage.setItem('access', '1')
+                } else if (auth === 'admin' || auth === 'perform') {
+                    sessionStorage.setItem('access', '2')
+                } else if (auth === 'super') {
+                    sessionStorage.setItem('access', '3')
+                }
+                this.$router.push({
+                    name: 'home_index'
+                })
+            })
+            .catch((err: any) => {
+                this.replace = !this.replace
+                this.$config.auth_notice(err)
+            })
+    }
+
+    mounted() {
+        let windows: any = window;
+        windows.particlesJS.load('band', `${process.env.BASE_URL}particlesjs-config.json`);
+        this.$http.get(`${this.$config.gen}/fetch`)
+            .then((res: { data: { reg: number; }; }) => {
+                if (res.data.reg === 1) {
+                    this.switchCode = true;
+                }
+            })
+            .catch((err: any) => this.$config.err_notice(this, err));
+    }
+}
 </script>
