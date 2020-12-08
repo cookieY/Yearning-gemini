1c2b7c23bd9d147755179ca1e4d4d97ff785c51a
HenryYee <henry@yearning.io>
2020-12-08 11:41:31 +0800

    修复状态异常的问题 修复加载spin异常的问题 修复mysql版本判断异常的问题

--- src/login.vue
+++ src/login.vue
@@ -33,7 +33,7 @@ a:visited {
         <nav class="navbar  bg-primary fixed-top navbar-transparent " color-on-scroll="400">
             <div class="container">
                 <div>
-                    <a class="navbar-brand"> {{ $t('version') }}: v2.3.1 Interstellar </a>
+                    <a class="navbar-brand"> {{ $t('version') }}: v2.3.1 Interstellar GA </a>
                     <Button v-if="switchCode" type="default" ghost @click="register = true">
                         {{ $t('sign') }}</Button
                     >
