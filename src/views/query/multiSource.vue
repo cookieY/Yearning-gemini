13ef46e11a6e81e4bbe3df29121f0cc630daef5d
HenryYee <henry@yearning.io>
2021-01-28 15:16:03 +0800

    format

--- src/views/query/multiSource.vue
+++ src/views/query/multiSource.vue
@@ -20,7 +20,7 @@
         </Card>
         <Modal v-model="openSource" title="数据源列表" :z-index="9999">
             <CellGroup @on-click="choiceSource">
-                <Cell v-for="i in fetchData.source" :title="i" :name="i" :key="i" />
+                <Cell v-for="i in fetchData.source" :title="i" :name="i" :key="i"/>
             </CellGroup>
             <div slot="footer">
                 <Button type="error" @click="deferReply">结束查询</Button>
@@ -37,81 +37,77 @@
 </template>
 
 <script lang="ts">
-    import query_sql from "@/views/query/querySql.vue";
-    import {Component, Mixins} from "vue-property-decorator";
-    import module_general from "@/store/modules/general";
-    import {FetchCommonGetApis} from "@/apis/commonApis";
-    import {AxiosResponse} from "axios";
-    import {Res} from "@/interface";
-    import {CommonPutApis} from "@/apis/queryApis";
-    import query_mixin from "@/mixins/query_mixin";
+import query_sql from "@/views/query/querySql.vue";
+import {Component, Mixins} from "vue-property-decorator";
+import module_general from "@/store/modules/general";
+import {FetchCommonGetApis} from "@/apis/commonApis";
+import {AxiosResponse} from "axios";
+import {Res} from "@/interface";
+import {CommonPutApis} from "@/apis/queryApis";
+import query_mixin from "@/mixins/query_mixin";
 
-    @Component({components: {query_sql}})
-    export default class multiSource extends Mixins(query_mixin) {
-        focus = '';
-        addSnippet = {
-            title: '',
-            text: ''
-        };
-        openSnippet = false;
-        openSource = false;
-        idc = '';
-        sourceList = [] as any;
+@Component({components: {query_sql}})
+export default class multiSource extends Mixins(query_mixin) {
+    focus = '';
+    addSnippet = {
+        title: '',
+        text: ''
+    };
+    openSnippet = false;
+    openSource = false;
+    idc = '';
+    sourceList = [] as any;
 
-        add_snip() {
-            module_general.snippetTag(this.addSnippet)
-        }
-
-        open_snip() {
-            this.openSnippet = true
-        }
-
-        handleTabRemove(name: any) {
-            let idx = this.sourceList.indexOf(name);
-            this.sourceList.splice(idx, 1);
-        }
-
-        openModel() {
-            this.openSource = true;
-            this.fetchSource()
-
-        }
-
-        choiceSource(vl: string) {
-            for (let i of this.sourceList) {
-                if (i === vl) {
-                    this.$Message.warning('不可重复打开相同数据源！');
-                    return
-                }
-            }
-            this.sourceList.push(vl);
-            this.openSource = false;
-            this.focus = vl
-        }
-
-        fetchSource() {
-            FetchCommonGetApis('source',{idc:this.idc,tp:'query'})
-                .then((res: AxiosResponse<Res>) => {
-                    if (res.data.payload.x === 'query') {
-                        this.fetchData.source = res.data.payload.source;
-                    } else {
-                        this.$config.notice('非法劫持参数！')
-                    }
-                })
-        }
-
-        mounted() {
-            CommonPutApis('status',null)
-                .then((res: AxiosResponse<Res>) => {
-                    if (res.data.payload.status !== 1) {
-                        this.$router.push({name: 'query'});
-                    } else {
-                        this.idc = res.data.payload.idc;
-                        this.openModel()
-                    }
-                })
-        }
+    add_snip() {
+        module_general.snippetTag(this.addSnippet)
     }
+
+    open_snip() {
+        this.openSnippet = true
+    }
+
+    handleTabRemove(name: any) {
+        let idx = this.sourceList.indexOf(name);
+        this.sourceList.splice(idx, 1);
+    }
+
+    openModel() {
+        this.openSource = true;
+        this.fetchSource()
+
+    }
+
+    choiceSource(vl: string) {
+        for (let i of this.sourceList) {
+            if (i === vl) {
+                this.$Message.warning('不可重复打开相同数据源！');
+                return
+            }
+        }
+        this.sourceList.push(vl);
+        this.openSource = false;
+        this.focus = vl
+    }
+
+    fetchSource() {
+        FetchCommonGetApis('source', {idc: this.idc, tp: 'query'})
+            .then((res: AxiosResponse<Res>) => {
+                this.fetchData.source = res.data.payload.source;
+            })
+    }
+
+    mounted() {
+        CommonPutApis('status', null)
+            .then((res: AxiosResponse<Res>) => {
+                if (res.data.payload.status !== 1) {
+                    this.$router.push({name: 'query'});
+                } else {
+                    this.idc = res.data.payload.idc;
+                    this.openModel()
+                }
+            })
+    }
+}
 </script>
 
 <style scoped>
